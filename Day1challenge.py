7fe6941031459a41554363fafa9d1c0191227acf
shivanim119911 <shivamvarpe1299mht@gmail.com>
2025-08-15 20:51:50 +0530

    Create Day1challenge.py
    Day 1 Challenge Code.

---
+++ Day1challenge.py
@@ -0,0 +1,19 @@
+def sort012(arr):
+    low, mid, high = 0, 0, len(arr) - 1
+
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:  # arr[mid] == 2
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+    return arr
+
+
+# Example usage
+arr = [0, 1, 2, 1, 0, 2, 1, 0]
+print(sort012(arr))
